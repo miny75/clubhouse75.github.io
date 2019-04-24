1abf89cc6cf05f648a1534fb8d6c83b57e050e51
miny75 <49933836+miny75@users.noreply.github.com>
2019-04-24 15:56:43 +0900

    Update images.php

--- images.php
+++ images.php
@@ -1,20 +1,20 @@
 [ 
     { 
-        "path": "img/img01.jpg",
+        "path": "img01.jpg",
         "name": "sansaisan", 
         "caption": "こっちは貫禄ありすぎ",
         "date": "sansaisan", 
         "memo": "こっちは貫禄ありすぎ" 
     }, 
     { 
-        "path": "img/img02.jpg", 
+        "path": "img02.jpg", 
         "name": "yukky_13dream", 
         "caption": "年明け富士山", 
         "date": "yukky_13dream", 
         "memo": "年明け富士山" 
     }, 
     { 
-        "path": "img/img03.jpg", 
+        "path": "img03.jpg", 
         "name": "maako", 
         "caption": "空と大地の境界線って、どのあたり？", 
         "date": "maako", 

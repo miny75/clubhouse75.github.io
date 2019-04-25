98dfdd903a82b99c15e0ee0e9a53df17f1b5e345
miny75 <49933836+miny75@users.noreply.github.com>
2019-04-25 20:30:10 +0900

    Add files via upload

--- images.php
+++ images.php
@@ -2,21 +2,21 @@
     { 
         "path": "img01.jpg",
         "name": "your dream", 
-        "caption": "初夏　高原の風　どこまでも続く空",
+        "caption": "The Samurai カックイー！",
         "date": "2018 July", 
         "memo": "初夏　高原の風　どこまでも続く空" 
     }, 
     { 
         "path": "img02.jpg", 
         "name": "57dream", 
-        "caption": "暖かな春　桜咲く", 
+        "caption": "甘く切ない　君が好き", 
         "date": "2019 March", 
         "memo": "暖かな春　桜咲く" 
     }, 
     { 
         "path": "img03.jpg", 
         "name": "Kangol", 
-        "caption": "Australiaでカンガーぇルー　　って", 
+        "caption": "草原の風", 
         "date": "2019 March", 
         "memo": "Australiaでカンガーぇルー　　って" 
     } 
